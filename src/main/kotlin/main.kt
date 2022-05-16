c37de35309795959d0cc3af855e4f37b11346adb
Ekaterina Papullo <vazhno@bk.ru>
2022-05-16 16:12:41 +0300

    Task is made

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,2 +1,14 @@
-class main {
+package ru.netology
+
+fun main() {
+
+    var likes = 631
+    val designationPeople: String
+    if (likes % 10 == 1 && likes % 100 != 11) {
+        designationPeople = "человеку"
+    } else designationPeople = "людям"
+    println("Понравилось $likes $designationPeople")
+
+
 }
+
